a270e2285480b40d0a1dcdd828247b31aa5adad5
clubvariete <nik@clubvariete.ch>
2022-09-14 03:18:47 +0200

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app" class="bg-green-600 py-56 min-h-screen">
     <div class="container mx-auto text-center px-8">
-      <img :src="logo">
+      <div class="fit">
+        <img :src="./assets/v2.png">
+      </div>
       <h2 class="text-3xl lg:text-6xl font-bold mb-2 text-white">Welcome to Your Vue App</h2>
       <h3 class="text-xl lg:text-4xl text-green-200">
         Deployed to
@@ -27,21 +29,14 @@
 <script>
 export default {
   name: "App",
-  data: function() {
-    return {
-      logo: require('./assets/v2.png')
-    }
-  }
-};
+  };
 </script>
 
 <style>
 #app {
   background-color: #000000;
 }
-img {
-  width: 200px;
-  height: 300px;
-  object-fit: contain;
+.fit img {
+    object-fit: contain;
 }
 </style>
