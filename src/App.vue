6c569cc5ee9a75692b6cdb1bd46aa86a28f342e3
clubvariete <nik@clubvariete.ch>
2022-09-14 03:11:02 +0200

    2

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app" class="bg-green-600 py-56 min-h-screen">
     <div class="container mx-auto text-center px-8">
-      <img :src="logo"
-        class="image-fit">
+      <img :src="logo">
       <h2 class="text-3xl lg:text-6xl font-bold mb-2 text-white">Welcome to Your Vue App</h2>
       <h3 class="text-xl lg:text-4xl text-green-200">
         Deployed to
@@ -39,6 +38,10 @@ export default {
 <style>
 #app {
   background-color: #000000;
-  background-image: url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%232c945c' fill-opacity='0.4'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+}
+img {
+  width: 200px;
+  height: 300px;
+  object-fit: contain;
 }
 </style>
